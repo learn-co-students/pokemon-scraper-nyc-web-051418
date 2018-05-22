ec9174f7789fcdc9d4d86c17bc6b9d0d20699628
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-21 20:01:48 -0400

    Solved lab w/o BONUS

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
+# saving, adding, removing, or changing anything about each Pokémon
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+
+  def initialize(hash)
+    #binding.pry
+    @id = hash[:id]
+    @name = hash[:name]
+    @type = hash[:type]
+    @db = hash[:db]
+    @@all << self
+  end
+  def self.all?
+    @@all
+  end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.results_as_hash = true
+    var = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)[0]
+    new_hash = {:id => var["id"], :name => var["name"],:type => var["type"]}
+    Pokemon.new(new_hash)
+  end
 end
