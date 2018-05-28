3ea5c1cc196d78cd78b2156750c0b869dc96381a
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-28 12:17:54 -0400

    Solved lab

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,16 @@
 # saving, adding, removing, or changing anything about each Pokémon
 require 'pry'
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   @@all = []
 
   def initialize(hash)
-    #binding.pry
+    # binding.pry
     @id = hash[:id]
     @name = hash[:name]
     @type = hash[:type]
     @db = hash[:db]
+    @hp = hash[:hp] #nil if empty
     @@all << self
   end
   def self.all?
@@ -22,7 +23,12 @@ class Pokemon
   def self.find(id, db)
     db.results_as_hash = true
     var = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)[0]
-    new_hash = {:id => var["id"], :name => var["name"],:type => var["type"]}
+    new_hash = {:id => var["id"], :name => var["name"],:type => var["type"], :db => db, :hp => var["hp"]}
     Pokemon.new(new_hash)
+  end
+
+  def alter_hp(new_hp, db)
+    var = self.id
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, var)
   end
 end
